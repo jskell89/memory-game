a58e7a334fccb831573830d1ac3a4467d5f2a159
Josh Skelly <jskell07@gmail.com>
2023-02-18 19:37:41 -0500

    Tracking missed matches and condition for number of tries until game over. New sounds added. resetBoard function added.

--- app.js
+++ app.js
@@ -53,23 +53,29 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const flipCardAudio = new Audio('sounds/Card-flip.mp3')
+const foundMatchAudio = new Audio('sounds/anime-wow-sound-effect.mp3')
+const buzzer = new Audio('sounds/Wheel-of-Fortune-Buzzer.mp3')
+const cheering = new Audio('sounds/Crowd-Cheering-Sound-Effect.mp3')
+const booing = new Audio('sounds/boo.wav')
 const grid = document.querySelector('.grid')
 const resultDisplay = document.querySelector('#result')
+const resultDisplayTwo = document.querySelector('#result2')
 var cardsChosen = []
 var cardsChosenId = []
 var cardsWon = []
+var cardsLost = []
 
 //create game board
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
         var card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
-        card.setAttribute('data-id', i)
+        card.setAttribute('id', i)
         card.setAttribute('class', 'back')
         card.addEventListener('click', flipCard)
-        
+
         grid.appendChild(card)
-  
+
     }
 }
 
@@ -79,7 +85,7 @@ function checkForMatch() {
     const optionOneId = cardsChosenId[0]
     const optionTwoId = cardsChosenId[1]
     if (cardsChosen[0] === cardsChosen[1]) {
-        alert('You found a match')
+        foundMatchAudio.play()
         cards[optionOneId].setAttribute('src', 'images/white.png')
         cards[optionTwoId].setAttribute('src', 'images/white.png')
         cards[optionOneId].setAttribute('class', 'blank')
@@ -90,28 +96,51 @@ function checkForMatch() {
         cards[optionTwoId].setAttribute('src', 'images/blank.png')
         cards[optionOneId].setAttribute('class', 'back')
         cards[optionTwoId].setAttribute('class', 'back')
-        alert('Sorry, try again')
+        cardsLost.push(cardsChosen)
+        buzzer.play()
     }
     cardsChosen = []
     cardsChosenId = []
     resultDisplay.textContent = cardsWon.length
-    if (cardsWon.length === cardArray.length/2){
+    resultDisplayTwo.textContent = cardsLost.length
+    if (cardsWon.length === cardArray.length / 2) {
         resultDisplay.textContent = 'Congratulations! All matches found!'
+        cheering.play()
+        setTimeout(resetBoard, 2000)
+    }
+    if (cardsLost.length === 8) {
+        resultDisplayTwo.textContent = 'Game Over!'
+        booing.play()
+        setTimeout(resetBoard, 2000)
     }
 }
-
 
 //flip your card
 function flipCard() {
     this.setAttribute('class', 'front')
     flipCardAudio.play()
-    var cardId = this.getAttribute('data-id')
+    var cardId = this.getAttribute('id')
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenId.push(cardId)
     this.setAttribute('src', cardArray[cardId].img)
     if (cardsChosen.length == 2) {
         setTimeout(checkForMatch, 500)
     }
+}
+
+function resetBoard() {
+   
+    for(let i=0; i < 12; i++){
+    grid.removeChild(document.getElementById(i)) 
+    }
+
+    cardArray.sort(() => 0.5 - Math.random())
+    createBoard()
+
+    cardsChosen = []
+    cardsChosenId = []
+    cardsWon = []
+    cardsLost = []
 }
 
 createBoard()
