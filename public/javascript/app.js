c9f8179ee6faea49a587107c69e0581617f9e566
JoshSkelly <josh.skelly@innismaggiore.com>
2025-03-08 15:40:42 -0500

    Moved UI functionality from Game class to UIManager

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -8,82 +8,29 @@ class Game {
     constructor() {
         this.player = new Player();
         this.audio = new AudioManager();
-        this.ui = new UIManager();
         this.cardManager = new CardManager();
+        this.uiCallbacks = {
+            onCardFlip: this.flipCard.bind(this),
+            onFormSubmit: this.handleFormSubmission.bind(this),
+            resetGame: this.resetGame.bind(this)
+        }
         this.rounds = 3;
         this.totalCards = this.cardManager.getCardCount();
         this.totalMatchesNeeded = this.totalCards * 0.5;
+        this.ui = new UIManager(this.player, this.cardManager, this.uiCallbacks, this.rounds, this.totalMatchesNeeded);
     }
 
     init() {
         this.cardManager.shuffle();
-        this.createLives();
-        this.bindEvents();
-    }
-
-    bindEvents() {
-        this.ui.elements.form.addEventListener('submit', (e) => this.handleFormSubmission(e));
-        this.ui.elements.startOverBtn.addEventListener('click', () => this.resetBoard());
-        this.ui.elements.quiteBtn.addEventListener('click', () => this.closeGame());
-        this.ui.elements.promptYesBtn.addEventListener('click', () => {
-            this.ui.elements.prompt.close();
-            this.ui.elements.overlay.style.display = 'none';
-            this.resetGame()
-        });
-        this.ui.elements.promptNoBtn.addEventListener('click', () => {
-            this.ui.elements.prompt.close();
-            this.ui.elements.overlay.style.display = 'none';
-            this.closeGame()
-        });
+        this.ui.createLives();
+        this.ui.bindEvents();
     }
 
     handleFormSubmission(e) {
         e.preventDefault();
         this.resetGame();
-        this.player.name = document.getElementById('username').value;
-        this.ui.elements.footer.textContent = `Welcome ${this.player.name}!`;
-        this.ui.elements.formWrapper.style.display = 'none';
-        this.ui.elements.game.style.display = 'grid';
+        this.ui.openGame();
     }
-
-    createLives() {
-        for (let i = 0; i < this.player.lives; i++) {
-            const life = document.createElement('img');
-            life.className = 'life';
-            life.src = 'images/brain-giphy.gif';
-            this.ui.elements.livesContainer.appendChild(life);
-        }
-    }
-
-    createBoard() {
-        for(let i = 0; i < this.totalCards; i++) {
-            const card = document.createElement('div');
-            const cardInner = document.createElement('div');
-            const cardFront = document.createElement('div');
-            const cardFrontImg = document.createElement('img');
-            const cardBack = document.createElement('div');
-            const cardBackImg = document.createElement('img');
-
-            card.className = 'card';
-            cardInner.className = 'card-inner';
-            cardInner.setAttribute('id', i.toString());
-            cardFront.className = 'card-front';
-            cardBack.className = 'card-back';
-            cardFrontImg.src = 'images/blank.png';
-            cardBackImg.src = this.cardManager.getCard(i).img;
-            cardBackImg.className = 'card-back-img';
-
-            cardFront.appendChild(cardFrontImg);
-            cardBack.appendChild(cardBackImg);
-            cardInner.appendChild(cardFront);
-            cardInner.appendChild(cardBack);
-            card.appendChild(cardInner);
-            this.ui.elements.cardContainer.appendChild(card);
-
-            cardInner.addEventListener('click', this.flipCard.bind(this, i));
-        }
-    }
-
 
     flipCard(cardID) {
         const card = document.getElementById(cardID);
@@ -135,12 +82,12 @@ class Game {
         if (this.player.matches === this.totalMatchesNeeded) {
             if (this.player.getTotalScore() <= this.totalCards) {
                 this.audio.cheering.play();
-                this.recordHighScore(this.player.getTotalScore()); // updates players highScores array and displays in table on front end.
+                this.ui.recordHighScore(this.player.getTotalScore());
 
                 if (this.player.getHighScores().length === 3) {
                     this.gameWin();
                 } else {
-                    this.roundWin(); // displays win message resets board for next round.
+                    this.ui.roundWin();
                 }
             }
         }
@@ -149,57 +96,22 @@ class Game {
             this.audio.match.pause();
             this.audio.booing.play();
             this.player.lives -= 1;
-            this.endLife(); // removes life indicators and checks if 0 lives left if so calls gameOver().
+            this.endLife();
 
             if (this.player.lives > 0) {
-                this.roundLose(); // displays lose message resets board for next round
+                this.ui.roundLose();
             }
         }
 
-        // clear chosenCards array and update running scoreboard.
         this.player.chosenCards = [];
-        this.updateScoreBoard();
+        this.ui.updateScoreBoard();
     }
 
-    recordHighScore(score) {
-        this.player.addHighScore(score);
-        this.ui.elements.scoreTable.insertAdjacentHTML('beforeend',
-            `<tr class="row">
-                <td>${score}</td>
-            </tr>`
-        );
-    }
-
-    updateScoreBoard() {
-        this.ui.elements.matches.textContent = this.player.matches;
-        this.ui.elements.misses.textContent = this.player.misses;
-        this.ui.elements.total.textContent = this.player.getTotalScore().toString();
-        this.ui.elements.turnsRemaining.textContent = (this.totalCards - this.player.getTotalScore()).toString();
-        this.ui.elements.accuracy.textContent = this.player.getAccuracy();
-    }
-
-    // sets display to none on indicators for lives and calls gameOver if no lives left.
     endLife() {
-        let lives = document.querySelectorAll('.life');
-        lives[this.player.lives].style.display = 'none';
-
+        this.ui.removeLife();
         if (this.player.lives === 0) {
             this.gameOver();
         }
-    }
-
-    roundWin() {
-        this.ui.elements.cardContainer.style.fontSize = "xx-large"
-        this.ui.elements.cardContainer.style.color = 'green';
-        this.ui.elements.cardContainer.textContent = 'Good Job!';
-        setTimeout(this.resetBoard.bind(this), 3000);
-    }
-
-    roundLose() {
-        this.ui.elements.cardContainer.style.fontSize = "xx-large";
-        this.ui.elements.cardContainer.style.color = 'red';
-        this.ui.elements.cardContainer.innerText = "You lost a life. \nTry Again!";
-        setTimeout(this.resetBoard.bind(this), 3000);
     }
 
     gameWin() {
@@ -208,94 +120,31 @@ class Game {
             spread: 1000,
             origin: { y: 0.3 }
         });
-        this.statAnimation();
         this.audio.winner.play();
-        this.openPrompt();
+        this.ui.showGameWin();
     }
 
     gameOver() {
-        this.ui.elements.cardContainer.style.color = "red";
-        this.ui.elements.cardContainer.innerText = "Game Over!";
         this.audio.booing.pause();
         this.audio.youSuck.play();
-
-        setTimeout(this.openPrompt.bind(this), 3000);
-    }
-
-    openPrompt() {
-        this.ui.elements.overlay.style.display = 'block';
-        this.ui.elements.prompt.open = true;
-    }
-
-    async statAnimation () {
-        const stats = [
-            { text: "High Score: " + this.player.getHighestScore().toString() },
-            { text: "Average Score: " + this.player.getAverageScore().toString() },
-            { text: "Accuracy: " + (((this.totalMatchesNeeded * this.rounds) / (this.player.getHighScores().reduce((accumulator, currentValue) => accumulator + currentValue, 0))) * 100).toFixed().toString() + "%" }
-        ];
-
-        const animationDelay = 1300;
-
-        for (const stat of stats) {
-            const p = document.createElement('p');
-            p.textContent = stat.text;
-            p.className = 'stat-item';
-            this.ui.elements.stats.append(p);
-
-            await new Promise(resolve => setTimeout(resolve, animationDelay));
-        }
-
-       this.ui.elements.btnGroup.classList.add('fade-in');
+        this.ui.showGameOver();
     }
 
     resetLives() {
-        const lives = this.ui.elements.livesContainer.querySelectorAll('.life');
-        lives.forEach((life) => {life.style.display = 'inline'});
         this.player.lives = 3;
+        this.ui.addLives();
     }
 
     resetScores() {
         this.player.clearHighScores();
-        this.clearScoreTable();
+        this.ui.clearScoreTable();
     }
 
     resetGame() {
         this.resetLives();
         this.resetScores();
-        this.resetBoard();
-        this.ui.elements.stats.replaceChildren();
-        this.ui.elements.btnGroup.classList.remove('fade-in');
-    }
-
-    clearScoreTable() {
-        const rows = this.ui.elements.scoreTable.querySelectorAll('.row');
-        rows.forEach(row => row.remove());
-    }
-
-    resetBoard() {
-        prompt.open = false;
-        if (this.player.getHighScores().length < 3 || this.player.lives > 0) {
-
-            this.ui.elements.cardContainer.textContent = "";
-            this.ui.elements.misses.textContent = "";
-            this.ui.elements.matches.textContent = "";
-            this.ui.elements.total.textContent = "";
-            this.ui.elements.turnsRemaining.textContent = this.totalCards.toString();
-            this.ui.elements.accuracy.textContent = "";
-
-            this.player.reset();
-            this.cardManager.shuffle();
-            this.createBoard();
-        }
-    }
-
-    // hides game shows login form. login form submit resets game.
-    closeGame() {
-        this.ui.elements.game.style.display = "none";
-        this.ui.elements.formWrapper.style.display = "block";
-        document.getElementById('username').value = "";
-        this.player.name = "";
-        this.ui.elements.cardContainer.replaceChildren();
+        this.ui.resetBoard();
+        this.ui.clearPrompt();
     }
 
     debugStats = () => {
--- public/javascript/modules/UIManager.js
+++ public/javascript/modules/UIManager.js
@@ -1,6 +1,14 @@
 // client/modules/UIManager.js
+import confetti from "canvas-confetti";
+
 export class UIManager {
-    constructor() {
+    constructor(player, cardManager, callbacks, rounds, totalMatchesNeeded) {
+        this.player = player;
+        this.cardManager = cardManager;
+        this.callbacks = callbacks;
+        this.rounds = rounds;
+        this.totalMatchesNeeded = totalMatchesNeeded;
+        this.totalCards = this.cardManager.getCardCount();
         this.elements = {
             overlay: document.querySelector('.overlay'),
             cardContainer: document.querySelector('.card-container'),
@@ -26,5 +34,177 @@ export class UIManager {
         }
     }
 
-    // UI manipulation methods
+    bindEvents() {
+        this.elements.form.addEventListener('submit', (e) => this.callbacks.onFormSubmit(e));
+        this.elements.startOverBtn.addEventListener('click', () => this.resetBoard());
+        this.elements.quiteBtn.addEventListener('click', () => this.closeGame());
+        this.elements.promptYesBtn.addEventListener('click', () => {
+            this.elements.prompt.close();
+            this.elements.overlay.style.display = 'none';
+            this.callbacks.resetGame();
+        });
+        this.elements.promptNoBtn.addEventListener('click', () => {
+            this.elements.prompt.close();
+            this.elements.overlay.style.display = 'none';
+            this.closeGame()
+        });
+    }
+
+    createLives() {
+        for (let i = 0; i < this.player.lives; i++) {
+            const life = document.createElement('img');
+            life.className = 'life';
+            life.src = 'images/brain-giphy.gif';
+            this.elements.livesContainer.appendChild(life);
+        }
+    }
+
+    removeLife() {
+        let lives = this.elements.livesContainer.querySelectorAll('.life');
+        lives[this.player.lives].style.display = 'none';
+    }
+
+    addLives() {
+        const lives = this.elements.livesContainer.querySelectorAll('.life');
+        lives.forEach(life => { life.style.display = 'inline'});
+    }
+
+    createBoard() {
+        for(let i = 0; i < this.totalCards; i++) {
+            const card = document.createElement('div');
+            const cardInner = document.createElement('div');
+            const cardFront = document.createElement('div');
+            const cardFrontImg = document.createElement('img');
+            const cardBack = document.createElement('div');
+            const cardBackImg = document.createElement('img');
+
+            card.className = 'card';
+            cardInner.className = 'card-inner';
+            cardInner.setAttribute('id', i.toString());
+            cardFront.className = 'card-front';
+            cardBack.className = 'card-back';
+            cardFrontImg.src = 'images/blank.png';
+            cardBackImg.src = this.cardManager.getCard(i).img;
+            cardBackImg.className = 'card-back-img';
+
+            cardFront.appendChild(cardFrontImg);
+            cardBack.appendChild(cardBackImg);
+            cardInner.appendChild(cardFront);
+            cardInner.appendChild(cardBack);
+            card.appendChild(cardInner);
+            this.elements.cardContainer.appendChild(card);
+
+            cardInner.addEventListener('click', () => this.callbacks.onCardFlip(i));
+        }
+    }
+
+    recordHighScore(score) {
+        this.player.addHighScore(score);
+        this.elements.scoreTable.insertAdjacentHTML('beforeend',
+            `<tr class="row">
+                <td>${score}</td>
+            </tr>`
+        );
+    }
+
+    updateScoreBoard() {
+        this.elements.matches.textContent = this.player.matches;
+        this.elements.misses.textContent = this.player.misses;
+        this.elements.total.textContent = this.player.getTotalScore().toString();
+        this.elements.turnsRemaining.textContent = (this.totalCards - this.player.getTotalScore()).toString();
+        this.elements.accuracy.textContent = this.player.getAccuracy();
+    }
+
+    roundWin() {
+        this.elements.cardContainer.style.fontSize = "xx-large"
+        this.elements.cardContainer.style.color = 'green';
+        this.elements.cardContainer.textContent = 'Good Job!';
+        setTimeout(this.resetBoard.bind(this), 3000);
+    }
+
+    roundLose() {
+        this.elements.cardContainer.style.fontSize = "xx-large";
+        this.elements.cardContainer.style.color = 'red';
+        this.elements.cardContainer.innerText = "You lost a life. \nTry Again!";
+        setTimeout(this.resetBoard.bind(this), 3000);
+    }
+
+    resetBoard() {
+        prompt.open = false;
+        if (this.player.getHighScores().length < 3 || this.player.lives > 0) {
+
+            this.elements.cardContainer.textContent = "";
+            this.elements.misses.textContent = "";
+            this.elements.matches.textContent = "";
+            this.elements.total.textContent = "";
+            this.elements.turnsRemaining.textContent = this.totalCards.toString();
+            this.elements.accuracy.textContent = "";
+
+            this.player.reset();
+            this.cardManager.shuffle();
+            this.createBoard();
+        }
+    }
+
+    showGameWin() {
+        this.statAnimation();
+        this.openPrompt();
+    }
+
+    showGameOver() {
+        this.elements.cardContainer.style.color = "red";
+        this.elements.cardContainer.innerText = "Game Over!";
+        this.elements.btnGroup.style.opacity = "1";
+        setTimeout(this.openPrompt.bind(this), 3000);
+    }
+
+    openPrompt() {
+        this.elements.overlay.style.display = 'block';
+        this.elements.prompt.open = true;
+    }
+
+    clearPrompt() {
+        this.elements.stats.replaceChildren();
+        this.elements.btnGroup.classList.remove('fade-in');
+    }
+
+    async statAnimation () {
+        const stats = [
+            { text: "High Score: " + this.player.getHighestScore().toString() },
+            { text: "Average Score: " + this.player.getAverageScore().toString() },
+            { text: "Accuracy: " + (((this.totalMatchesNeeded * this.rounds) / (this.player.getHighScores().reduce((accumulator, currentValue) => accumulator + currentValue, 0))) * 100).toFixed().toString() + "%" }
+        ];
+
+        const animationDelay = 1300;
+
+        for (const stat of stats) {
+            const p = document.createElement('p');
+            p.textContent = stat.text;
+            p.className = 'stat-item';
+            this.elements.stats.append(p);
+
+            await new Promise(resolve => setTimeout(resolve, animationDelay));
+        }
+
+        this.elements.btnGroup.classList.add('fade-in');
+    }
+
+    clearScoreTable() {
+        const rows = this.elements.scoreTable.querySelectorAll('.row');
+        rows.forEach(row => row.remove());
+    }
+
+    openGame() {
+        this.player.name = document.getElementById('username').value;
+        this.elements.footer.textContent = `Welcome ${this.player.name}!`;
+        this.elements.formWrapper.style.display = 'none';
+        this.elements.game.style.display = 'grid';
+    }
+    closeGame() {
+        this.elements.game.style.display = "none";
+        this.elements.formWrapper.style.display = "block";
+        document.getElementById('username').value = "";
+        this.player.name = "";
+        this.elements.cardContainer.replaceChildren();
+    }
 }
